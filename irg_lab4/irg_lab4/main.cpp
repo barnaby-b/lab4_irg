9d14d334d4e26041b108e531f7531162bb947582
Borna Berta <berta.borna@gmail.com>
2018-05-31 18:07:16 +0200

    removed redundancies from main

--- irg_lab4/irg_lab4/main.cpp
+++ irg_lab4/irg_lab4/main.cpp
@@ -1,12 +1,7 @@
-#include<iostream>
-
-#include<GL/glut.h>
-
 #include "GlobalRenderer.hpp"
 
 int main(int argc, char *argv[])
 {
-	using namespace std;
 	GlobalRenderer::instance().renderer.init(argc, argv, "Laboratorijska vjezba 4");
 	return 0;
 }
